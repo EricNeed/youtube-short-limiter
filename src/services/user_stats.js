a1029b2fea68a3786222f22e993ac164c9e99895
Eric Ni <6600271@spsd.sk.ca>
2026-04-30 15:24:09 -0600

    trying to implement expo-router to the project

--- src/services/user_stats.js
+++ src/services/user_stats.js
@@ -1,19 +1,32 @@
-import { getUsageStats, hasUsageStatsPermission } from "expo-android-usagestats";
+import { getInstalledApps, hasUsageStatsPermission } from "expo-android-usagestats";
 
 export async function getUserStatsTest(){
     console.log("getting the permission info...");
     console.log( await hasUsageStatsPermission());
 
-    // console.log("getting the app list...");
-    // console.log( await getInstalledApps());
+    console.log("getting the app list...");
+    console.log( await getInstalledApps());
 
-    console.log("getting all user stats...");
-    let time_now = Date.now();
-    let apptime = await getUsageStats(time_now - 600000, time_now);
-    //for(const app of apptime){}
-    console.log(apptime);
+    // console.log("getting all user stats...");
+    // let time_now = Date.now();
+    // let apptime = await getUsageStats(time_now - 600000, time_now);
+    // //for(const app of apptime){}
+    // console.log(apptime);
 }
 
-export async function getAppFromList(){
 
+/** 
+get the app usage of specified apps
+@param timeMin the interval in minute before now that i want to check the usage
+@param appList the list of package names of app in string
+@returns the list of minutes the same order of appList
+*/
+export async function getStatsFiltered(timeMin, appList){
+    return 
+}
+
+
+
+export async function getAppFromList(){
+    return await getInstalledApps();
 }
