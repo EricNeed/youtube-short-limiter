9c0c8bf29b9de5026379ec428ae0e0295b21ad74
Eric Ni <6600271@spsd.sk.ca>
2026-05-08 12:10:20 -0600

    trying out the flatlist to display all the apps

--- src/app/(modals)/select_app.js
+++ src/app/(modals)/select_app.js
@@ -1,19 +1,33 @@
 import { Stack } from "expo-router";
-import { Text, View } from "react-native";
-import { Switch } from "react-native-paper";
+import { FlatList, Text, View } from "react-native";
 import { mainStyle } from "../../components/scheme_style";
 
 export default function selectApp(){
 
+    const hi = ["1", "2", "3", "4", "5"];
+
+    //flatlist call this function to draw each button
+    const displayButton = ({item}) => (
+        <View>
+            {/* <Switch/> */}
+            <Text style={mainStyle.text}>hi</Text>
+        </View>
+    );
+
     return <View style={mainStyle.container}>
+
         <Stack.Screen options={{
             title: 'Select Apps',
             headerStyle: { backgroundColor: mainStyle.container.backgroundColor },
             headerTintColor: mainStyle.text.color,
         }}/>
         <Text style={mainStyle.text}>hi</Text>
-        <Switch value={isOn} onValueChange={onToggleSwitch} />
-        <Switch value={isOn} onValueChange={onToggleSwitch} />
+        
+        <FlatList
+            data={hi}
+            renderItem={displayButton}
+        />
+
     </View>
     
 }
