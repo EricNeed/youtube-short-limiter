cae500b36415b5531698a623f72cc624c22e3246
Eric Ni <6600271@spsd.sk.ca>
2026-05-20 15:46:52 -0600

    trying out some stuff with modal

--- src/app/(modals)/no_perm.js
+++ src/app/(modals)/no_perm.js
@@ -1,7 +1,10 @@
 import { Text, View } from "react-native";
 
 export default function noUsageOrNotificationPerm(){
-    return <View>
-        <Text>hi</Text>
+    return <View style={{backgroundColor: '#ffffff46', flex:1}}>
+        <Text style={{color: '#ffffff'}}>hi</Text>
+        <View>
+            <Text style={{color: '#ffffff'}}> hellllo</Text>
+        </View>
     </View>
 }
