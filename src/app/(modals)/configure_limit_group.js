dad93d21ef72996f19e4d95173de97d17059adab
Eric Ni <6600271@spsd.sk.ca>
2026-06-04 15:25:15 -0600

    keep working on configure app menu

--- src/app/(modals)/configure_limit_group.js
+++ src/app/(modals)/configure_limit_group.js
@@ -1,8 +1,9 @@
 import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
-import { Button } from 'react-native-paper';
+import { Button, IconButton } from 'react-native-paper';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { mainStyle } from "../../components/scheme_style";
+import { symbolCache } from "../../components/symbol_cache";
 import { getSelectedApps } from "../../utils/settings/tracked_apps";
 
 export default function configureGroupPage(){
@@ -13,8 +14,13 @@ export default function configureGroupPage(){
     return <SafeAreaView style={{flex:1}}>
         <View style={{flex:1}}></View>
         <View style={{flex:2, backgroundColor:'#333333aa', padding: 5, borderColor: '#555555', borderTopWidth:4, borderRightWidth:4, borderLeftWidth:4}}>
-            <Text style={{color: '#ffffff', fontSize: mainStyle.text.fontSize*1.8}} >{trackingGroups[groupID].name}</Text>
+            <View style={{flexDirection: "row", alignSelf: "stretch", justifyContent: "space-between",}}>
+                <Text style={{color: '#ffffff', fontSize: mainStyle.text.fontSize*1.8}} >{trackingGroups[groupID].name}</Text>
+                <IconButton icon={({color}) => (symbolCache.close(color))} mode='contained'/>
+            </View>
             <Button mode="elevated" onPress={() => console.log('Pressed')}>hi</Button>
         </View>
     </SafeAreaView>
 }
+
+//<IconButton icon={() => (<SymbolView name={{android: 'settings', web: 'settings'}} fallback={<Text>?⚙️?</Text>}/>)} mode='contained-tonal'/>
--- src/components/symbol_cache.js
+++ src/components/symbol_cache.js
@@ -7,4 +7,5 @@ export const symbolCache = {
     home: (color, size=symbolSize) => (<SymbolView name={{android: 'home', web: 'home'}} size={size} tintColor={color} fallback={<Text>?🏠?</Text>}/>),
     settings: (color, size=symbolSize) => (<SymbolView name={{android: 'settings', web: 'settings'}} size={size} tintColor={color} fallback={<Text>?⚙️?</Text>}/>),
     dataUsage: (color, size=symbolSize) => (<SymbolView name={{android: 'data_usage', web: 'data_usage'}} size={size} tintColor={color} fallback={<Text>?📉?</Text>}/>),
+    close: (color, size=symbolSize) => (<SymbolView name={{android: 'close', web: 'close'}} size={size} tintColor={color} fallback={<Text>?📉?</Text>}/>),
 };
